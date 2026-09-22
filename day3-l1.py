56a38cf34ed98ff31496fefa613f9e865bb01f45
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2026-09-22 15:16:25 +0530

    Add day3-l1.py from PythonGuruji compiler

---
+++ day3-l1.py
@@ -0,0 +1,37 @@
+number = int(input("Enter the number: "))
+
+if number > 0:
+    print("it is a positive")
+elif number < 0:
+    print("it is a negative")
+else:
+    print("it is a Zero")
+    
+ 
+number = int(input("Enter the number: "))
+ 
+if number % 2 == 0:
+    print("it is a even number")
+else:
+    print("it is a odd number")
+    
+    
+age = int(input("Enter the Age: "))
+
+if age > 18:
+    print("you are eligible to vote")
+else:
+    print("you are a minor")
+    
+first_number = int(input("Enter the number: "))
+Second_number = int(input("Enter the number: "))
+
+if first_number > Second_number:
+     print("first_number is larger")
+elif first_number < Second_number:
+     print("Second_number is larger")
+else:
+    print("Both numbers are equal")
+                    
+                 
+            
