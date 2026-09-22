26e85eda83dbfbf704449970fc47315381e4f3b8
Rakshithgt <114352972+Rakshithgt@users.noreply.github.com>
2026-09-22 12:48:51 +0530

    Add day2practice.py from PythonGuruji compiler

---
+++ day2practice.py
@@ -0,0 +1,20 @@
+sentence = "Python is easy and Python is powerful"
+print(sentence.count("Python"))
+
+a = input("enter the word:")
+print(f"the word is {a}")
+
+print(f"the reversed word is {a[::-1]}")
+
+name = input("enter the name:")
+print(f"original name is {name}")
+print(f"cleaned name is {name.strip()}")
+print(f"uppercase name is {name.upper()}")
+print(f"lowercase name is {name.lower()}")
+print(f"titlecase name is {name.title()}")
+print(f"first character is {name[0]}")
+print(f"last character is {name[11]}")
+print(f"the reversed character is {name[::-1]}")
+print(f"the character in the name is {(len(name))}")
+
+     
